261422f2d10c3bce5c2108e3ec1c1df96a248037
julio19salazar <juliosalazar378@gmail.com>
2021-09-24 17:44:39 -0500

    Agregar archivos de proyecto.

---
+++ ActividadDosByJulio/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using ActividadDosByJulio.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActividadDosByJulio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hola, para continuar ingresa la dirección /Home/PaginaPromedio";
+        }
+        public IActionResult PaginaPromedio(PaginaPromedioViewModel vm)
+        {
+           
+            return View(vm);
+        }
+    }
+}
